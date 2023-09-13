35ac2ee3f591ad7337dbc8a19080e66531456352
Chris Zuber <shgysk8zer0@gmail.com>
2023-09-12 17:06:45 -0700

    Update API to handle modules

--- api/firebase-config.js
+++ api/firebase-config.js
@@ -1,5 +1,5 @@
 /* eslint-env node */
-exports.handler = async event => {
+export const handler = async event => {
 	const isDevMode = process.env.NETLIFY_DEV === 'true' || process.env.PULL_REQUEST !== undefined;
 	const origin = new URL(event.headers.origin || event.headers.referer).origin;
 	const allowedOrigins = [
