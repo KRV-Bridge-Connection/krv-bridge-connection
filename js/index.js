367d07316db5c0f61b69b9ab06768a3cb5f78762
Chris Zuber <shgysk8zer0@gmail.com>
2024-04-16 11:17:58 -0700

    Install updates to fix build issues

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 import '@shgysk8zer0/kazoo/theme-cookie.js';
-import { getGooglePolicy } from '@shgysk8zer0/kazoo/trust-policies.js';
+// import { getGooglePolicy } from '@shgysk8zer0/kazoo/trust-policies.js';
 import { ready, toggleClass, css, on } from '@shgysk8zer0/kazoo/dom.js';
 import { debounce } from '@shgysk8zer0/kazoo/events.js';
 import { init } from '@shgysk8zer0/kazoo/data-handlers.js';
 import { send } from '@shgysk8zer0/kazoo/slack.js';
-import { importGa, externalHandler, telHandler, mailtoHandler } from '@shgysk8zer0/kazoo/google-analytics.js';
+// import { importGa, externalHandler, telHandler, mailtoHandler } from '@shgysk8zer0/kazoo/google-analytics.js';
 // import { submitHandler } from './contact-demo.js';
-import { GA } from './consts.js';
+// import { GA } from './consts.js';
 import { navigate } from './functions.js';
 import './components.js';
 import './user.js';
@@ -29,21 +29,21 @@ toggleClass([document.documentElement], {
 	'no-js': false,
 });
 
-if (typeof GA === 'string' && GA.length !== 0) {
-	scheduler.postTask(() => {
-		importGa(GA, {}, { policy: getGooglePolicy() }).then(async ({ ga, hasGa }) => {
-			if (hasGa()) {
-				ga('create', GA, 'auto');
-				ga('set', 'transport', 'beacon');
-				ga('send', 'pageview');
+// if (typeof GA === 'string' && GA.length !== 0) {
+// 	scheduler.postTask(() => {
+// 		importGa(GA, {}, { policy: getGooglePolicy() }).then(async ({ ga, hasGa }) => {
+// 			if (hasGa()) {
+// 				ga('create', GA, 'auto');
+// 				ga('set', 'transport', 'beacon');
+// 				ga('send', 'pageview');
 
-				on('a[rel~="external"]', ['click'], externalHandler, { passive: true, capture: true });
-				on('a[href^="tel:"]', ['click'], telHandler, { passive: true, capture: true });
-				on('a[href^="mailto:"]', ['click'], mailtoHandler, { passive: true, capture: true });
-			}
-		});
-	}, { priority: 'background' });
-}
+// 				on('a[rel~="external"]', ['click'], externalHandler, { passive: true, capture: true });
+// 				on('a[href^="tel:"]', ['click'], telHandler, { passive: true, capture: true });
+// 				on('a[href^="mailto:"]', ['click'], mailtoHandler, { passive: true, capture: true });
+// 			}
+// 		});
+// 	}, { priority: 'background' });
+// }
 
 Promise.all([
 	customElements.whenDefined('install-prompt'),
