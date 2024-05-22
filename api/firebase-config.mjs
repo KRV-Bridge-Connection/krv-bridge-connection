73f04482c4ae2590f780cfd62f92ff73fdaa933d
Chris Zuber <shgysk8zer0@gmail.com>
2024-05-22 11:56:01 -0700

    Update firebase-config.mjs
    Swap import order... Should hint at if this is a bug with just the one or all.

--- api/firebase-config.mjs
+++ api/firebase-config.mjs
@@ -1,7 +1,7 @@
 /* eslint-env node */
 
-import { HTTPError } from '@shgysk8zer0/http/error.js';
 import { FORBIDDEN, INTERNAL_SERVER_ERROR, NOT_IMPLEMENTED } from '@shgysk8zer0/consts/status.js';
+import { HTTPError } from '@shgysk8zer0/http/error.js';
 
 const ALLOWED_ORIGINS = [
 	'https://krvbridge.org',
