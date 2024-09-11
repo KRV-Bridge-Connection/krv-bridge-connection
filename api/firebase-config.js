397a6991e0791e7d324a6d08c19326b21ab5c05a
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-10 18:15:23 -0700

    Simplify things...

--- api/firebase-config.js
+++ api/firebase-config.js
@@ -1,7 +1,6 @@
 /* eslint-env node */
 import '@shgysk8zer0/polyfills';
 import { createHandler } from '@shgysk8zer0/lambda-http/handler';
-import { HTTPInternalServerError } from '@shgysk8zer0/lambda-http/error';
 
 const ALLOWED_ORIGINS = [
 	'https://krvbridge.org',
@@ -9,24 +8,20 @@ const ALLOWED_ORIGINS = [
 ];
 
 if (typeof globalThis?.process?.env?.BASE_URL === 'string') {
-	ALLOWED_ORIGINS.push(URL.parse(globalThis.process.env.BASE_URL)?.origin);
+	ALLOWED_ORIGINS.push(URL.parse(process.env.BASE_URL)?.origin);
 }
 
 export default createHandler({
 	async get() {
-		if (! ('process' in globalThis)) {
-			throw new HTTPInternalServerError('process is not defined.');
-		} else {
-			return Response.json({
-				apiKey: globalThis.process.env.FIREBASE_API_KEY,
-				authDomain: globalThis.process.env.FIREBASE_AUTH_DOMAIN,
-				projectId: globalThis.process.env.FIREBASE_PROJECT_ID,
-				storageBucket: globalThis.process.env.FIREBASE_STORAGE_BUCKET,
-				messagingSenderId: globalThis.process.env.FIREBASE_MESSAGE_SENDER_ID,
-				appId: globalThis.process.env.FIREBASE_APP_ID,
-				measurementId: globalThis.process.env.FIREBASE_MEASUREMENT_ID,
-			});
-		}
+		return Response.json({
+			apiKey: process.env.FIREBASE_API_KEY,
+			authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+			projectId: process.env.FIREBASE_PROJECT_ID,
+			storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+			messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+			appId: process.env.FIREBASE_APP_ID,
+			measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+		});
 	}
 }, {
 	allowOrigins: ALLOWED_ORIGINS,
