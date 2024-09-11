dcfb59106425f7f6881f497a42612a7d8111139d
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-10 18:04:13 -0700

    Update firebase-config function

--- api/firebase-config.js
+++ api/firebase-config.js
@@ -8,35 +8,27 @@ const ALLOWED_ORIGINS = [
 	'https://www.krvbridge.org',
 ];
 
-if (typeof globalThis.process.env.BASE_URL === 'string') {
-	ALLOWED_ORIGINS.push(new URL(globalThis.process.env.BASE_URL).origin);
+if (typeof globalThis?.process?.env?.BASE_URL === 'string') {
+	ALLOWED_ORIGINS.push(URL.parse(globalThis.process.env.BASE_URL)?.origin);
 }
 
 export default createHandler({
 	async get() {
-		try {
-			if (! ('process' in globalThis)) {
-				throw new HTTPInternalServerError('`process` is not defined.');
-			} else {
-
-				return Response.json({
-					apiKey: globalThis.process.env.FIREBASE_API_KEY,
-					authDomain: globalThis.process.env.FIREBASE_AUTH_DOMAIN,
-					projectId: globalThis.process.env.FIREBASE_PROJECT_ID,
-					storageBucket: globalThis.process.env.FIREBASE_STORAGE_BUCKET,
-					messagingSenderId: globalThis.process.env.FIREBASE_MESSAGE_SENDER_ID,
-					appId: globalThis.process.env.FIREBASE_APP_ID,
-					measurementId: globalThis.process.env.FIREBASE_MEASUREMENT_ID,
-					// Add other Firebase configuration properties as needed
-				});
-			}
-		} catch(err) {
-			console.error(err);
-			throw err;
+		if (! ('process' in globalThis)) {
+			throw new HTTPInternalServerError('process is not defined.');
+		} else {
+			return Response.json({
+				apiKey: globalThis.process.env.FIREBASE_API_KEY,
+				authDomain: globalThis.process.env.FIREBASE_AUTH_DOMAIN,
+				projectId: globalThis.process.env.FIREBASE_PROJECT_ID,
+				storageBucket: globalThis.process.env.FIREBASE_STORAGE_BUCKET,
+				messagingSenderId: globalThis.process.env.FIREBASE_MESSAGE_SENDER_ID,
+				appId: globalThis.process.env.FIREBASE_APP_ID,
+				measurementId: globalThis.process.env.FIREBASE_MEASUREMENT_ID,
+			});
 		}
 	}
 }, {
 	allowOrigins: ALLOWED_ORIGINS,
 	requireCors: true,
-	// requireJWT: true,
 });
