fa4d5c6e4ea7fdb2276582c6d468c6b6d9bfa6f3
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-15 10:13:23 -0700

    Revert to just sending response

--- api/firebase-config.js
+++ api/firebase-config.js
@@ -1,16 +1,11 @@
 /* eslint-env node */
-import { createHandler } from '@shgysk8zer0/lambda-http';
 
-export default createHandler({
-	async get() {
-		return Response.json({
-			apiKey: process.env.FIREBASE_API_KEY,
-			authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-			projectId: process.env.FIREBASE_PROJECT_ID,
-			storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-			messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
-			appId: process.env.FIREBASE_APP_ID,
-			measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-		});
-	}
+export default async () => Response.json({
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
+	measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 });
