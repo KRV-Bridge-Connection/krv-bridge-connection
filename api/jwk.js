2d1229f64da6350288d353821cc53e37fd59f186
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-15 10:18:53 -0700

    Update public key endpoint

--- api/jwk.js
+++ api/jwk.js
@@ -1,23 +1,27 @@
-import { createHandler, HTTPInternalServerError } from '@shgysk8zer0/lambda-http';
 import { importJWK } from '@shgysk8zer0/jwk-utils/jwk';
 import { readFile } from 'node:fs/promises';
+import { INTERNAL_SERVER_ERROR } from '@shgysk8zer0/consts/status.js';
 
 async function getPublicKey() {
 	const keyData = JSON.parse(await readFile('_data/jwk.json', { encoding: 'utf-8' }));
 	return await importJWK(keyData);
 }
 
-export default createHandler({
-	async get() {
-		const publicKey = await getPublicKey();
+function sendError(message, status = INTERNAL_SERVER_ERROR, details = null) {
+	return Response.json({
+		error: { message, status, details }
+	}, { status });
+}
 
-		if (publicKey instanceof CryptoKey) {
-			const data = await crypto.subtle.exportKey('jwk', publicKey);
-			return Response.json(data);
-		} else if (publicKey instanceof Error) {
-			throw new HTTPInternalServerError('Could not access public JWK.', { cause: publicKey });
-		} else {
-			throw new HTTPInternalServerError('Could not access public JWK.');
-		}
+export default async () => {
+	const publicKey = await getPublicKey();
+
+	if (publicKey instanceof CryptoKey) {
+		const data = await crypto.subtle.exportKey('jwk', publicKey);
+		return Response.json(data);
+	} else if (publicKey instanceof Error) {
+		return sendError('Could not access public JWK.', INTERNAL_SERVER_ERROR);
+	} else {
+		return sendError('Could not access public JWK.', INTERNAL_SERVER_ERROR);
 	}
-});
+};
