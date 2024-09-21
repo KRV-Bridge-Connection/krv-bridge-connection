2c0d82d81e76ecab97688a1d3654b9e94804662d
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-21 11:19:55 -0700

    Try importing *only* `@shgysk8zer0/lambda-http`
    `createHandler` will be the only import here...

--- api/test.js
+++ api/test.js
@@ -1,14 +1,15 @@
 /* eslint-env node */
-import '@shgysk8zer0/polyfills';
+// import '@shgysk8zer0/polyfills';
 import { createHandler } from '@shgysk8zer0/lambda-http';
-import { readFile } from 'node:fs/promises';
+// import { readFile } from 'node:fs/promises';
 
 export default createHandler({
 	async get() {
 		try {
-			const path = new URL(import.meta.url)?.pathname;
-			const content = await readFile(path, { encoding: 'utf-8' });
-			return new Response([content], { headers: { 'Content-Type': 'text/plain', 'X-Path': path }});
+			// const path = new URL(import.meta.url)?.pathname;
+			// const content = await readFile(path, { encoding: 'utf-8' });
+			const content = 'Hello, World!';
+			return new Response([content], { headers: { 'Content-Type': 'text/plain' }});
 		} catch(err) {
 			console.error(err);
 			return Response.json(err, { status: 500 });
