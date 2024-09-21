6b995be3eba0b5e99a3096c8cfe5e5fe182d7f81
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-21 11:38:31 -0700

    Remove all imports
    This is just to verify that the issue is with `createHandler`

--- api/test.js
+++ api/test.js
@@ -1,8 +1,5 @@
 /* eslint-env node */
-import { createHandler } from '@shgysk8zer0/lambda-http';
 
-export default createHandler({
-	async get() {
-		return new Response(['Hello, World!'], { headers: { 'Content-Type': 'text/plain' }});
-	}
-});
+export default async () => {
+	return new Response(['Hello, World!'], { headers: { 'Content-Type': 'text/plain' }});
+};
