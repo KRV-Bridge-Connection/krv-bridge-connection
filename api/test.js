0cffc596f16010ca6462778e780ef64f450f1372
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-21 11:34:38 -0700

    Go to the absolute most basic...

--- api/test.js
+++ api/test.js
@@ -1,18 +1,8 @@
 /* eslint-env node */
-// import '@shgysk8zer0/polyfills';
-import { createHandler, HTTPInternalServerError } from '@shgysk8zer0/lambda-http';
-// import { readFile } from 'node:fs/promises';
+import { createHandler } from '@shgysk8zer0/lambda-http';
 
 export default createHandler({
 	async get() {
-		try {
-			// const path = new URL(import.meta.url)?.pathname;
-			// const content = await readFile(path, { encoding: 'utf-8' });
-			const content = 'Hello, World!';
-			return new Response([content], { headers: { 'Content-Type': 'text/plain' }});
-		} catch(err) {
-			// console.error(err);
-			throw new HTTPInternalServerError('Something went wrong', { cause: err });
-		}
+		return new Response(['Hello, World!'], { headers: { 'Content-Type': 'text/plain' }});
 	}
 });
