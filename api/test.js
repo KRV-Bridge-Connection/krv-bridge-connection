20e3c680de9cad0b7ae8eb4d0638c6a8b689c3bd
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-21 08:05:35 -0700

    Simplify test

--- api/test.js
+++ api/test.js
@@ -5,16 +5,9 @@ import { readFile } from 'node:fs/promises';
 export default createHandler({
 	async get(req) {
 		try {
-			const url = new URL(req.url);
-
-			if (url.searchParams.has('mimes')) {
-				const mimes = await import('@shgysk8zer0/consts/mimes.js');
-				return Response.json(mimes);
-			} else {
-				const path = URL.parse(import.meta.url)?.pathname;
-				const content = await readFile(path, { encoding: 'utf-8' });
-				return new Response([content], { headers: { 'Content-Type': 'text/plain' }});
-			}
+			const path = URL.parse(import.meta.url)?.pathname;
+			const content = await readFile(path, { encoding: 'utf-8' });
+			return new Response([content], { headers: { 'Content-Type': 'text/plain' }});
 		} catch(err) {
 			console.error(err);
 			return Response.error();
