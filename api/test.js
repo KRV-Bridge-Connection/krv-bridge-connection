dc811114a98e14b949a9c8e7af4f13d1a463f22b
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-21 08:27:10 -0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,21 +1,12 @@
+/* eslint-env node */
+import '@shgysk8zer0/polyfills';
 import { createHandler } from '@shgysk8zer0/lambda-http';
 import { readFile } from 'node:fs/promises';
-
 
 export default createHandler({
 	async get() {
 		try {
-			if (! (URL.parse instanceof Function)) {
-				URL.parse = function parse(url, base) {
-					try {
-						return new URL(url, base);
-					} catch {
-						return null;
-					}
-				};
-			}
-
-			const path = URL.parse(import.meta.url)?.pathname;
+			const path = new URL(import.meta.url)?.pathname;
 			const content = await readFile(path, { encoding: 'utf-8' });
 			return new Response([content], { headers: { 'Content-Type': 'text/plain', 'X-Path': path }});
 		} catch(err) {
