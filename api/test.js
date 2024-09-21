7fa06d01492acba6d10df6853b3d710dbb4cc836
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-21 08:20:54 -0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,26 +1,26 @@
-// import '@shgysk8zer0/polyfills';
 import { createHandler } from '@shgysk8zer0/lambda-http';
 import { readFile } from 'node:fs/promises';
 
-if (! (URL.parse instanceof Function)) {
-	URL.parse = function parse(url, base) {
-		try {
-			return new URL(url, base);
-		} catch {
-			return null;
-		}
-	};
-}
 
 export default createHandler({
 	async get() {
 		try {
+			if (! (URL.parse instanceof Function)) {
+				URL.parse = function parse(url, base) {
+					try {
+						return new URL(url, base);
+					} catch {
+						return null;
+					}
+				};
+			}
+
 			const path = URL.parse(import.meta.url)?.pathname;
 			const content = await readFile(path, { encoding: 'utf-8' });
-			return new Response([content], { headers: { 'Content-Type': 'text/plain' }});
+			return new Response([content], { headers: { 'Content-Type': 'text/plain', 'X-Path': path }});
 		} catch(err) {
 			console.error(err);
-			return Response.error();
+			return Response.json(err, { status: 500 });
 		}
 	}
 });
