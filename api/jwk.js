fb78d750ba0e760b426ee91f79214ae9d65cf11a
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-22 10:46:35 -0700

    Finish up changes
    Done with testing, so let's get this to production.

--- api/jwk.js
+++ api/jwk.js
@@ -1,27 +1,23 @@
 import { importJWK, MIME_TYPE } from '@shgysk8zer0/jwk-utils';
 import { readFile } from 'node:fs/promises';
-// import { INTERNAL_SERVER_ERROR } from '@shgysk8zer0/consts/status.js';
+import { createHandler, HTTPInternalServerError } from '@shgysk8zer0/lambda-http';
 
 async function getPublicKey() {
 	const keyData = JSON.parse(await readFile('_data/jwk.json', { encoding: 'utf-8' }));
 	return await importJWK(keyData);
 }
 
-function sendError(message, status = 500, details = null) {
-	return Response.json({
-		error: { message, status, details }
-	}, { status });
-}
+export default createHandler({
+	async get() {
+		const publicKey = await getPublicKey();
 
-export default async () => {
-	const publicKey = await getPublicKey();
-
-	if (publicKey instanceof CryptoKey) {
-		const data = await crypto.subtle.exportKey('jwk', publicKey);
-		return Response.json(data, { headers: { 'Content-Type': MIME_TYPE }});
-	} else if (publicKey instanceof Error) {
-		return sendError('Could not access public JWK.', 500);
-	} else {
-		return sendError('Could not access public JWK.', 500);
+		if (publicKey instanceof CryptoKey) {
+			const data = await crypto.subtle.exportKey('jwk', publicKey);
+			return Response.json(data, { headers: { 'Content-Type': MIME_TYPE }});
+		} else if (publicKey instanceof Error) {
+			throw new HTTPInternalServerError('Could not access public JWK.');
+		} else {
+			throw new HTTPInternalServerError('Could not access public JWK.');
+		}
 	}
-};
+});
