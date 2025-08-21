7d5c9db440605feb40676ea5ebd25dfe325ed3a7
Chris Zuber <shgysk8zer0@gmail.com>
2025-08-21 12:31:20 -0700

    Update pantry.js

--- js/routes/pantry.js
+++ js/routes/pantry.js
@@ -36,6 +36,7 @@ const OPENING_HOURS = Date.now() < HOURS_CHANGE
 	];
 
 const CLOSED = [
+ '2025-08-21', // Ran empty
 	'2025-09-01', // Labor Day
 	'2025-11-11', // Veterans Day
 	'2025-11-27', // Thanksgiving
