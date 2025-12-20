85e216da403e6c556385efb4f403d87b594b61e4
Chris Zuber <shgysk8zer0@gmail.com>
2025-12-19 16:45:10 -0800

    Update pantry.js

--- js/routes/pantry.js
+++ js/routes/pantry.js
@@ -95,8 +95,12 @@ const CLOSED = [
 	'2025-11-11', // Veterans Day
 	'2025-11-27', // Thanksgiving
 	'2025-11-28', // Day after Thanksgiving
+	'2025-12-14', // Christmas Eve
 	'2025-12-25', // Christmas
+	'2025-12-16', // Day after Christmas
+	'2025-12-31', // New Year's Eve
 	'2026-01-01', // New Year's Day
+	'2026-01-02', // Day after New Year's
 	'2026-01-19', // Martin Luther King Jr. Day
 	'2026-02-16', // Presidents' Day
 	'2026-05-25', // Memorial Day
