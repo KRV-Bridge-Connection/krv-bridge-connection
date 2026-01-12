c7cdcb1b53793443537e31a2c7634a0c84d92099
Chris Zuber <shgysk8zer0@gmail.com>
2026-01-12 11:24:46 -0800

    Add birthday field for household members in pantry
    Extended the maximum household size to 10 and updated both backend and frontend to collect and store each household member's name and birthday. The form now includes a date input for birthdays, and the API stores this information in the householdMembers array.

--- js/components/pantry.js
+++ js/components/pantry.js
@@ -8,6 +8,7 @@ export const HOUSEHOLD_CLASSNAME = 'household-member';
 export const HOUSEHOLD_MEMBER_CLASSNAME = 'household-member-item';
 export const HOUSEHOLD_LIST = 'pantry-household-list';
 export const ADD_HOUSEHOLD_MEMBER_ID = 'pantry-add-household-member-btn';
+const MAX_HOUSEHOLD_SIZE = 10;
 
 export const pantryAddHousehold = registerCallback('pantry:household:add', () => {
 	/**
@@ -45,11 +46,12 @@ export const pantryHouseholdSizeChange = registerCallback('pantry:household_size
 	}
 });
 
-export const getHouseholdSize = household => `<input type="number" name="household" id="${HOUSEHOLD_SIZE_ID}" class="input" placeholder="## *" min="1" max="8" inputmode="numeric" autocomplete="off" ${attr({ value: household })} required="" />`;
+export const getHouseholdSize = household => `<input type="number" name="household" id="${HOUSEHOLD_SIZE_ID}" class="input" placeholder="## *" min="1" max="${MAX_HOUSEHOLD_SIZE}" inputmode="numeric" autocomplete="off" ${attr({ value: household })} required="" />`;
 
 export const getPantryHouseholdTemplate = ({ signal }) => `<template id="${HOUSEHOLD_TEMPLATE_ID}">
 	<li class="flex row ${HOUSEHOLD_MEMBER_CLASSNAME}">
-		<input name="person[]" required="" class="household-member input" minlength="3" ${onChange}="${pantryHouseholdSizeChange}" ${signalAttr}="${signal}">
+		<input type="text" name="person[]" required="" class="household-member input" minlength="3" ${onChange}="${pantryHouseholdSizeChange}" ${signalAttr}="${signal}">
+		<input type="date" name="bDay[]" class="household-member input" required="" />
 		<button type="button" class="btn btn-danger" title="Remove Household Member" aria-label="Remove Household Member" ${onClick}="${pantryRemoveHousehold}" ${signalAttr}="${signal}">
 			<svg fill="currentColor" height="16" width="16" role="presentation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 16">
 				<path fill-rule="evenodd" d="M11 2H9c0-.55-.45-1-1-1H5c-.55 0-1 .45-1 1H2c-.55 0-1 .45-1 1v1c0 .55.45 1 1 1v9c0 .55.45 1 1 1h7c.55 0 1-.45 1-1V5c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 12H3V5h1v8h1V5h1v8h1V5h1v8h1V5h1v9zm1-10H2V3h9v1z"></path>
