a985da287e63b387b952c47012ed55dd8ae0e72b
Chris Zuber <shgysk8zer0@gmail.com>
2026-01-25 11:51:23 -0800

    Add guest sign-in dialog to pantry distribution
    Introduces a new sign-in dialog for guests, allowing entry of name and household size. Adds logic for calculating points based on household size and updates appointment state accordingly. Also refactors form submission handling and updates UI to include the new sign-in option.

--- js/routes/pantry-distribution.js
+++ js/routes/pantry-distribution.js
@@ -22,6 +22,22 @@ const ADD_ITEM_ID = 'pantry-manual';
 const NON_FOOD_ID = 'pantry-non-food';
 const MAX_PER_ITEM = 99;
 const OTHER_ELS = ['nav', 'sidebar', 'footer'];
+const PTS = [
+	30, // 1
+	60, // 2
+	80, // 3
+	95, // 4
+	110, // 5
+	120, // 6
+	125, // 7
+	130, // 8
+	135, // 9
+	140, //10
+];
+
+const MAX_HOUSEHOLD = PTS.length;
+const BASE_POINTS = 5;
+const SIGN_UP_ID = 'pantry-sign-up-dialog';
 
 const numberClass = 'small-numeric';
 const JWT_EXP = /^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_=]*$/;
@@ -190,6 +206,10 @@ const numberFormatter = new Intl.NumberFormat('en', {
 	minimumIntegerDigits: 1,
 });
 
+function _getPoints(household) {
+	return PTS[Math.min(Math.max(parseInt(household), 1), MAX_HOUSEHOLD) - 1];
+}
+
 const unlock = () => screen.orientation.unlock();
 const lockScreen = registerCallback('pantry:distribution:orientation-lock', async ({ currentTarget }) => {
 	await document.getElementById('main').requestFullscreen();
@@ -230,6 +250,7 @@ async function setAppt({ id, name, givenName, familyName, points, household}) {
 	document.getElementById('pantry-household').value = household;
 	document.getElementById('appt-details').hidden = false;
 	document.getElementById('cart-grand-total').dataset.points = points;
+	history.replaceState({...history.state ?? {}, id, name, givenName, familyName, points, household }, '', location.href);
 }
 
 const storageKey = '_lastSync:pantry:inventory';
@@ -380,19 +401,9 @@ const submitHandler = registerCallback('pantry:distribution:submit', async event
 			}
 
 			status.reset({ idle: false });
-			const resp = await fetch(PANTRY_ENDPOINT, {
-				method: 'POST',
-				body: new FormData(target),
-			}).catch(() => Response.error());
-
-			if (resp.ok) {
-				setCart([]);
-				target.reset();
-				status.resolve();
-			} else {
-				alert('Error completing transaction.');
-				status.reject();
-			}
+			setCart([]);
+			target.reset();
+			status.resolve();
 		}
 	} catch(err) {
 		status.reject();
@@ -444,6 +455,42 @@ const addNonFood = registerCallback('pantry:add:nonfood', async ({ target }) => 
 		}
 	}
 });
+
+const signUpSubmit = registerCallback('pantry:distribution:sign-up:submit', async event => {
+	event.preventDefault();;
+	const { target, submitter } = event;
+
+	try {
+		submitter.disabled = true;
+		const data = new FormData(target);
+		const name = ['givenName', 'additionalName', 'familyName', 'suffix']
+			.map(field => data.has(field) ? data.get(field).trim() : null)
+			.filter(field => typeof field === 'string' && field.length !== 0)
+			.join(' ');
+
+		const household = parseInt(data.get('household'));
+		const normalTrip = ! data.has('extraTrip');
+		const points = normalTrip ? _getPoints(household) : Math.min(Math.max(household, 1), PTS.length) * BASE_POINTS;
+
+		await setAppt({
+			id: crypto.randomUUID(),
+			name,
+			givenName: data.get('givenName'),
+			additionalName: data.get('additionalName'),
+			familyName: data.get('familyName'),
+			household,
+			points,
+		});
+
+		target.reset();
+	} catch(err) {
+		alert(err);
+	} finally {
+		submitter.disabled = false;
+	}
+});
+
+const signUpRest = registerCallback('pantry:distribution:sign-up:reset', event => event.target.closest('dialog').requestClose());
 
 const scrollToEnd = registerCallback('pantry:distribution:scroll-to-end', () => {
 	document.getElementById('pantry-cart').scrollIntoView({ behavior: 'smooth', block: 'end' });
@@ -752,6 +799,7 @@ export default async function({
 			<button type="button" class="btn btn-secondary" popovertarget="${ADD_ITEM_ID}" popovertargetaction="show">Add Item</button>
 			<button type="button" class="btn btn-secondary" command="show-modal" commandfor="${NON_FOOD_ID}">Add Non-Food Item</button>
 			<!--<button type="button" class="btn btn-primary" command="show-modal" commandfor="nuscan-modal">Scanner</button>-->
+			<button type="button" class="btn btn-primary" command="show-modal" commandfor="${SIGN_UP_ID}">Sign-In</button>
 			<button type="button" class="btn btn-secondary" ${onClick}="${lockScreen}" ${signalAttr}="${sig}" aria-label="Lock Orientation" ${attr({ disabled: ! (screen?.orientation?.lock instanceof Function) })}>
 				<svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16" class="icon" fill="currentColor" aria-hidden="true">
 					<path fill-rule="evenodd" d="M13 10h1v3c0 .547-.453 1-1 1h-3v-1h3v-3zM1 10H0v3c0 .547.453 1 1 1h3v-1H1v-3zm0-7h3V2H1c-.547 0-1 .453-1 1v3h1V3zm1 1h10v8H2V4zm2 6h6V6H4v4zm6-8v1h3v3h1V3c0-.547-.453-1-1-1h-3z"/>
@@ -781,6 +829,61 @@ export default async function({
 				<use href="/img/icons.svg#x"></use>
 			</svg>
 		</button>
+	</dialog>
+	<dialog id="${SIGN_UP_ID}">
+		<form ${onSubmit}="${signUpSubmit}" ${onReset}="${signUpRest}" ${signalAttr}="${sig}">
+			<fieldset>
+				<legend>Sign-In Guest</legend>
+				<div class="form-group flex wrap space-between">
+					<span>
+						<label for="pantry-given-name" class="input-label required">First Name</label>
+						<input type="text" name="givenName" id="pantry-given-name" class="input" placeholder="First name *" autocomplete="given-name" required="" />
+					</span>
+					<span>
+						<label for="pantry-additional-name" class="input-label">Middle Name</label>
+						<input type="text" name="additionalName" id="pantry-additional-name" class="input" placeholder="Middle name" autocomplete="additional-name" />
+					</span>
+					<span>
+						<label for="pantry-family-name" class="input-label required">Last Name</label>
+						<input type="text" name="familyName" id="pantry-family-name" class="input" placeholder="Last name *" autocomplete="family-name" required="" />
+					</span>
+					<span>
+						<label for="pantry-name-suffix" class="input-label">Suffix</label>
+						<input type="text"
+							name="suffix"
+							id="pantry-name-suffix"
+							class="input"
+							autocomplete="honorific-suffix"
+							list="suffix-options"
+							size="3"
+							minlength="2"
+							placeholder="Jr., Sr., III, etc." />
+						<datalist id="suffix-options">
+							<option value="Jr">
+							<option value="Sr">
+							<option value="II">
+							<option value="III">
+							<option value="IV">
+						</datalist>
+					</span>
+				</div>
+				<div class="form-group">
+					<label for="pantry-household" class="input-label required">Household Size</label>
+					<input type="number" name="household" id="pantry-household" class="input" min="1" max="${MAX_HOUSEHOLD}" placeholder="##" required="" />
+				</div>
+			</legend>
+			<div>
+				<button type="submit" class="btn btn-success">
+					<span>Sign-In</span>
+				</button>
+				<button type="button" class="btn btn-secondary" command="request-close" commandfor="${SIGN_UP_ID}">
+					<span>Close</span>
+				</button>
+				<button type="reset" class="btn btn-danger">
+					<span>Cancel</span>
+				</button>
+			</div>
+		</form>
 	</dialog>
 	<form id="${ADD_ITEM_ID}" popover="manual" ${onSubmit}="${addItemSubmit}" ${onReset}="${addItemReset}" ${signalAttr}="${sig}">
 		<div>
