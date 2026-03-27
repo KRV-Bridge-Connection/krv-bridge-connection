6f785c512b76fa1e7b14880c2105efc3b7e41da6
Chris Zuber <shgysk8zer0@gmail.com>
2026-03-27 13:36:39 -0700

    Update handling of Google Calendar

--- api/gcal.js
+++ api/gcal.js
@@ -1,14 +1,18 @@
 import { createHandler, HTTPBadGatewayError } from '@shgysk8zer0/lambda-http';
 import { url } from '@aegisjsproject/url/parser.js';
+import { openSecretStoreFile } from '@aegisjsproject/secret-store';
+import { getSecretKey } from '@shgysk8zer0/aes-gcm';
 
 const CALENDARS = {
 	pantry: 'c_16733423f0da780704fd694ee47fbfbd2e7d0aaac0f076464b49e02794c4f412@group.calendar.google.com',
 };
 
-const getCalURL = cal => cal in CALENDARS ? url`https://www.googleapis.com/calendar/v3/calendars/${CALENDARS[cal]}/events?key=${process.env.GOOGLE_CALENDAR_API_KEY}` : null;
+const getCalURL = (cal, key) => cal in CALENDARS ? url`https://www.googleapis.com/calendar/v3/calendars/${CALENDARS[cal]}/events?key=${key}` : null;
 
 async function getCal(cal = 'pantry', { signal } = {}) {
-	const url = getCalURL(cal?.toLowerCase());
+	const key = await getSecretKey();
+	const [secrets] = await openSecretStoreFile(key, '_data/secrets.json');
+	const url = getCalURL(cal?.toLowerCase(), await secrets.GOOGLE_CALENDAR_API_KEY);
 
 	if (url instanceof URL) {
 		const resp = await fetch(url, { headers: { Accept: 'application/json', signal }});
