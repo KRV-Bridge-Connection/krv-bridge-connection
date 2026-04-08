be523714f0f48a4f183e511e1512befb19e77bbf
Chris Zuber <shgysk8zer0@gmail.com>
2026-04-08 08:38:06 -0700

    Revert "Disable PostCSS cascade layers"
    This reverts commit 253c36b7c298519cdd291864c9ceb7af749637ea.

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,3 @@
-import postcssImport from '@shgysk8zer0/postcss-import';
-import postcssPresetEnv from 'postcss-preset-env';
-import pcDiscardComments from 'postcss-discard-comments';
-import pcCustomProperties from 'postcss-custom-properties';
-import pcMediaMinMax from 'postcss-media-minmax';
-import CSSNano from 'cssnano';
-import postcssNesting  from 'postcss-nesting';
+import { getConfig } from '@shgysk8zer0/css-utils/postcss';
 
-export const plugins = [
-	postcssImport, postcssPresetEnv({
-		features: {
-			'cascade-layers': false
-		}
-	}), pcDiscardComments, pcCustomProperties,
-	pcMediaMinMax, CSSNano, postcssNesting,
-];
-
-export default { map: { inline: false }, plugins };
+export default getConfig({ inline: false });
