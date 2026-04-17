5469946f96380dcd89a3665ae2cdd1ac39060b92
Chris Zuber <shgysk8zer0@gmail.com>
2026-04-17 10:28:20 -0700

    Update prefetching

--- worker.mjs
+++ worker.mjs
@@ -18,13 +18,31 @@ try {
 				'/js/index.min.js', '/css/index.min.css', '/img/icons.svg', '/img/favicon.svg',
 				'/js/routes/pantry.js', '/js/routes/partners.js', '/js/routes/volunteer.js',
 				'/js/components/g-cal.js', '/img/apple-touch-icon.png', '/img/icon-192.png',
-				'/js/routes/404.js', '/js/routes/contact.js'
+				'/js/routes/404.js', '/js/routes/contact.js', '/js/consts.js', '/js/functions.js',
 			].map(path => URL.parse(path, location.origin)),
 		},
 		{
 			name: 'unpkg',
 			strategy: 'cache-first',
 			pattern: new URLPattern({ baseURL: 'https://unpkg.com/', pathname: '/*' }),
+			prefetch: [
+				'{{ importmap.imports["@aegisjsproject/router"] }}',
+				'{{ importmap.imports["@aegisjsproject/idb"] }}',
+				'{{ importmap.imports["@aegisjsproject/core/"] }}parsers/html.js',
+				'{{ importmap.imports["@aegisjsproject/core/"] }}parsers/css.js',
+				'{{ importmap.imports["@aegisjsproject/core/"] }}stringify.js',
+				'{{ importmap.imports["@aegisjsproject/core/"] }}dom.js',
+				'{{ importmap.imports["@aegisjsproject/markdown"] }}',
+				'{{ importmap.imports["@aegisjsproject/url/"] }}search.js',
+				'{{ importmap.imports["@aegisjsproject/callback-registry/"] }}callbackRegistry.js',
+				'{{ importmap.imports["@aegisjsproject/callback-registry/"] }}callbacks.js',
+				'{{ importmap.imports["@aegisjsproject/callback-registry/"] }}events.js',
+				'{{ importmap.imports["@aegisjsproject/state/"] }}state.js',
+				'{{ importmap.imports["@aegisjsproject/state"] }}',
+				'{{ importmap.imports["@aegisjsproject/firebase-account-routes"] }}',
+				'{{ importmap.imports["@aegisjsproject/firebase-account-routes/"] }}auth.js',
+				'{{ importmap.imports["@aegisjsproject/disposable-registry"] }}',
+			]
 		},
 		{
 			name: 'imgur',
@@ -34,7 +52,7 @@ try {
 		{
 			name: 'google-fonts',
 			strategy: 'cache-first',
-			pattern: new URLPattern({ baseURL: 'https://fonts.googleapis.com/' }),
+			pattern: new URLPattern({ baseURL: 'https://fonts.googleapis.com/', pathname: '/*' }),
 		}
 	]);
 } catch(err) {
