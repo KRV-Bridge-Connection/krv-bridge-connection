44b7fadd6ca362721d5e50a7a9de208eb83e0d85
Chris Zuber <shgysk8zer0@gmail.com>
2026-09-08 14:43:08 -0700

    Update echo.js

--- api/echo.js
+++ api/echo.js
@@ -1,3 +1,29 @@
-import * as EXTS from '@shgysk8zer0/consts/exts.js';
+import { readdir, stat, readFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { join, dirname } from 'node:path';
 
-export default () => Response.json(EXTS);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const handler = async () => {
+	try {
+		const files = await readdir(__dirname);
+		const fileContents = {};
+
+		for (const file of files) {
+			const filePath = join(__dirname, file);
+			const fileStat = await stat(filePath);
+
+			if (fileStat.isFile()) {
+				fileContents[file] = await readFile(filePath, 'utf8');
+			}
+		}
+
+		return Response.json({
+			directory: __dirname,
+			files: fileContents,
+		});
+	} catch (error) {
+		return Response.json({ error: { message: error.message }});
+	}
+};
